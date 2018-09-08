1eb1dfb2c6057124e475488b559ccb0a958ba9de
icecreamsoldier <mariana.9000@hotmail.com>
2018-09-08 16:04:04 -0500

    Finished lab4

--- Lab4/normalize.c
+++ Lab4/normalize.c
@@ -12,7 +12,7 @@
 #define SIZE 	1000
 #define WIDTH 	68
 
-int values[SIZE];
+float values[SIZE][SIZE];
 
 int main(int argc, char *argv[]) {
   if(argc != 3) {
@@ -23,9 +23,14 @@ int main(int argc, char *argv[]) {
   DIR* dir2;
   struct dirent* direntry;
   char filename[PATH_MAX + NAME_MAX + 1];
+  char filenameout[PATH_MAX + NAME_MAX + 1];
   FILE *fd_in;
-  int time, intersections, size, i;
+  FILE *fd_out;
+  int time, intersections, size, i, j=0;
   float num;
+  float max = 0.0, min = 0.0;
+  int count = 0;
+  int aux = 0;
 
 
   if((dir = opendir(argv[1])) == NULL || (dir2 = opendir(argv[2])) == NULL ) {
@@ -36,7 +41,6 @@ int main(int argc, char *argv[]) {
   while ((direntry = readdir(dir)) != NULL) {
     if(strcmp(direntry->d_name, ".") != 0 && strcmp(direntry->d_name, "..") != 0){
       if(direntry->d_type != DT_DIR) {
-        // printf("%s\n", direntry->d_name);
         sprintf(filename, "%s/%s", argv[1], direntry->d_name);
         fd_in = fopen(filename, "r");
         if (fd_in == NULL) {
@@ -48,12 +52,43 @@ int main(int argc, char *argv[]) {
     		size = intersections * intersections;
     		for (i = 0; i < size; i++) {
     			fscanf(fd_in, ",%f", &num);
-    			values[i] = (int)num;
-          printf("%i ", values[i]);
+    			values[i][j] = num;
+          // printf("%f", values[i][j]);
+          if(values[i][j] > max) {
+            max = values[i][j];
+          }
+          if(values[i][j] < min) {
+            min = values[i][j];
+          }
+          aux++;
     		}
+        j++;
+        count = j;
     	 }
+       printf("\n%f", max);
+       // printf("\n%f\n", min);
+       sprintf(filenameout, "%s/%s", argv[2], direntry->d_name);
+       fd_out = fopen(filenameout, "w");
+       if (fd_out == NULL) {
+         perror(argv[0]);
+         return -1;
+       }
+       for(j = 0; j < count; j++) {
+         for(i = 0; i < size; i++) {
+           values[i][j] = ((values[i][j]-min)/(max-min));
+           fprintf(fd_out, "%f,",values[i][j]);
+           // printf("%f ", values[i][j]);
+         }
+       }
+       // printf("%i\n", aux);
+      // fwrite(values, sizeof(float), aux, fd_out);
       fclose(fd_in);
+      fclose(fd_out);
+      j = 0;
+      max = 0;
+      min = 0;
       }
     }
   }
+  return 0;
 }
